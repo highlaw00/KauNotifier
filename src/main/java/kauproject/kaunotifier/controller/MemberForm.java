ff978bae4892742e2c1515855cc3c9b92bfc1de5
Highlaw <choiyool00@gmail.com>
2024-01-15 20:53:59 +0900

    (feat) 구독 조회 시 사용되는 멤버 DTO 구현
    - 해당 DTO는 구독 조회 시 이메일과 이름을 입력받기 때문에 두 필드(email, name)를 가집니다.

---
+++ src/main/java/kauproject/kaunotifier/controller/MemberForm.java
@@ -0,0 +1,11 @@
+package kauproject.kaunotifier.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+    private String name;
+    private String email;
+}

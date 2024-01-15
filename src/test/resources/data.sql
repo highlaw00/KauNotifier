ac35a956a5d4a35093b30565f500048e881d03b2
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 11:49:34 +0900

    (chord) 테스트용 DDL, DML 추가

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+insert into source(name, description, url) values
+('normal_notice', '일반공지', 'https://kau.ac.kr/web/pages/gc32172b.do'),
+('college_nontice', '학사공지', 'https://kau.ac.kr/web/pages/gc14561b.do'),
+('aerospace_mechanical_engineering', '항공우주 및 기계공학부', 'http://college.kau.ac.kr/web/pages/gc1986b.do'),
+('aerospace_electronics_engineering', '항공전자정보공학부', 'http://college.kau.ac.kr/web/pages/gc23761b.do'),
+('aeronautical_flight', '항공운항학과', 'http://college.kau.ac.kr/web/pages/gc61682b.do'),
+('air_transporation_logistics', '항공교통물류학부', 'http://college.kau.ac.kr/web/pages/gc93464b.do'),
+('business_administration', '경영학과', 'http://college.kau.ac.kr/web/pages/gc99805b.do'),
+('faculty_of_business_administration', '경영학부', 'http://college.kau.ac.kr/web/pages/gc93337b.do'),
+('new_materials_engineering', '신소재공학과', 'http://college.kau.ac.kr/web/pages/gc46806b.do'),
+('software_engineering', '소프트웨어학과', 'http://college.kau.ac.kr/web/pages/gc911b.do'),
+('smart_drone_engineering', '스마트드론공학과', 'http://college.kau.ac.kr/web/pages/gc13106b.do'),
+('liberal_studies', '자유전공학부 전계열', 'http://college.kau.ac.kr/web/pages/gc46051b.do'),
+('liberal_studies_engineering', '자유전공학부 공학계열', 'http://college.kau.ac.kr/web/pages/gc16458b.do');

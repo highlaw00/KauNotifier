7196cc425590a71cc921bb5d1644c1b480eead20
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:28:40 +0900

    (feat) 구독 서비스 구현
    - 구독 기능 구현
    	- 멤버와 출처가 주어졌을 때, Subscription 인스턴스(멤버, 출처를
    	  참조하는)를 생성하여 레포지토리로 전달합니다.

--- src/main/java/kauproject/kaunotifier/service/SubscriptionService.java
+++ src/main/java/kauproject/kaunotifier/service/SubscriptionService.java
@@ -1,2 +1,37 @@
-package kauproject.kaunotifier.service;public class SubscriptionService {
+package kauproject.kaunotifier.service;
+
+import kauproject.kaunotifier.domain.Member;
+import kauproject.kaunotifier.domain.Source;
+import kauproject.kaunotifier.domain.Subscription;
+import kauproject.kaunotifier.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionService {
+
+    private final SubscriptionRepository subscriptionRepository;
+
+    public void subscribe(Member member, List<Source> sourceList) {
+        List<Subscription> subscriptionList = new ArrayList<>();
+        for (Source source : sourceList) {
+            Subscription subscription = Subscription.createSubscription(member, source);
+            member.addSubscription(subscription);
+            subscriptionList.add(subscription);
+        }
+
+        subscriptionRepository.saveMany(subscriptionList);
+    }
+
+    public List<Subscription> findAll() {
+        return subscriptionRepository.findAll();
+    }
+
+    public List<Subscription> findSubscriptionOfMember(Member member) {
+        return subscriptionRepository.findAllByMember(member);
+    }
 }

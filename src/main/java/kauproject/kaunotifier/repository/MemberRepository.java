30afb68f439c4dbd21a8b05375807e2c4ecd5305
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:22:17 +0900

    (feat) 멤버 리포지토리 구현
    - JPA를 사용한 데이터베이스 접근
    - 회원 저장 기능
    - 회원 조회 기능 (ID 활용)
    - 회원 조회 기능 (Email, Name 활용)
    	- Email과 Name이 동일하지 않으면 회원이 조회되지 않습니다.
    	- 본인(회원)이 아닌 타인이 Email만을 사용해 구독 정보를 조회,
    	  수정하는 것을 막기 위함입니다.
    - 회원 전체 조회 기능

--- src/main/java/kauproject/kaunotifier/repository/MemberRepository.java
+++ src/main/java/kauproject/kaunotifier/repository/MemberRepository.java
@@ -5,13 +5,32 @@ import kauproject.kaunotifier.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
     private final EntityManager em;
 
-    void save(Member member) {
+    public Long save(Member member) {
         em.persist(member);
+        return member.getId();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public Optional<Member> findByNameAndEmail(String name, String email) {
+        return Optional.ofNullable(em.createQuery("select m from Member m where m.name = :name and m.email = :email", Member.class)
+                .setParameter("name", name)
+                .setParameter("email", email)
+                .getSingleResult());
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 }

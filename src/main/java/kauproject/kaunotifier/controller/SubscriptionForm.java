25706d92f7fd434c53cff6f20c464ba7e3378760
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:11:47 +0900

    (feat) 구독 정보를 담을 폼(DTO) 작성
    - 사용자로부터 이메일, 이름, 구독할 공지 출처를 받을 DTO 입니다.

---
+++ src/main/java/kauproject/kaunotifier/controller/SubscriptionForm.java
@@ -0,0 +1,18 @@
+package kauproject.kaunotifier.controller;
+
+import kauproject.kaunotifier.domain.Member;
+import kauproject.kaunotifier.domain.Source;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class SubscriptionForm {
+
+    private String name;
+    private String email;
+    private List<String> selectedSources = new ArrayList<>();
+}

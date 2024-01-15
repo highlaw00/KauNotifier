241749981c99aecf07161fb0242d2f3e402dc819
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:25:35 +0900

    (feat) 구독정보 레포지토리 구현
    - JPA를 사용한 데이터베이스 접근
    - 구독 정보 저장 기능
    	- 구독 정보에 멤버와 출처 정보가 모두 저장된 상태여야 합니다.
    - 다중 구독 정보 저장 기능
    - 단일 구독 조회 기능
    - 전체 구독 조회 기능
    - 특정 사용자의 구독 조회 기능
    	- 특정 사용자의 구독을 수정하는 로직을 위해 구현

--- src/main/java/kauproject/kaunotifier/repository/SubscriptionRepository.java
+++ src/main/java/kauproject/kaunotifier/repository/SubscriptionRepository.java
@@ -1,2 +1,47 @@
-package kauproject.kaunotifier.repository;public class SubscriptionRepository {
+package kauproject.kaunotifier.repository;
+
+import jakarta.persistence.EntityManager;
+import kauproject.kaunotifier.domain.Member;
+import kauproject.kaunotifier.domain.Subscription;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class SubscriptionRepository {
+
+    private final EntityManager em;
+
+    public Long save(Subscription subscription) {
+        em.persist(subscription);
+        return subscription.getId();
+    }
+
+    public List<Subscription> saveMany(List<Subscription> subscriptionList) {
+        for (Subscription subscription : subscriptionList) {
+            em.persist(subscription);
+        }
+        return subscriptionList;
+    }
+
+    public Optional<Subscription> findById(Long id) {
+        return Optional.ofNullable(em.find(Subscription.class, id));
+    }
+
+    public List<Subscription> findAll() {
+        return em.createQuery("select s from Subscription s", Subscription.class).getResultList();
+    }
+
+    /**
+     * 사용자 ID를 기준으로 구독 탐색
+     * @return
+     */
+    public List<Subscription> findAllByMember(Member member) {
+        return em.createQuery("select s from Subscription s where s.member.id = :id", Subscription.class)
+                .setParameter("id", member.getId())
+                .getResultList();
+    }
 }

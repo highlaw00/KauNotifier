315bcec5f903e8b7d4a764c0c3bc7ef93dd38821
Highlaw <choiyool00@gmail.com>
2024-01-15 20:58:08 +0900

    (feat) 구독 탐색 뷰페이지 매핑, 구독 조회 뷰페이지 매핑
    - `getFind()`: 구독 탐색 뷰페이지 반환
    - `findMember()`: 구독 내용 탐색 후 구독 내용이 존재하면 구독 조회 뷰페이지로 리다이렉션,
    아니라면 에러 페이지로 리다이렉션
    - `showSingle()`: 구독 조회 뷰페이지 반환

--- src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
+++ src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
@@ -1,5 +1,6 @@
 package kauproject.kaunotifier.controller;
 
+import kauproject.kaunotifier.domain.Member;
 import kauproject.kaunotifier.domain.Source;
 import kauproject.kaunotifier.repository.SourceRepository;
 import kauproject.kaunotifier.service.MemberService;
@@ -7,11 +8,10 @@ import kauproject.kaunotifier.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +23,7 @@ public class SubscribeController {
 
     @GetMapping("subscribe")
     public String subscribeHome(Model model) {
-        List<Source> sourceList = sourceRepository.findAll();
+        List<Source> sourceList = sourceRepository.findAllList();
         model.addAttribute("sources", sourceList);
         model.addAttribute("subscriptionForm", new SubscriptionForm());
         return "subscription/subscribe";
@@ -32,10 +32,64 @@ public class SubscribeController {
     @PostMapping("subscribe")
     public String subscribe(@ModelAttribute SubscriptionForm subscriptionForm) {
         String name = subscriptionForm.getName();
-        List<String> selectedSources = subscriptionForm.getSelectedSources();
-        System.out.println("name = " + name);
-        System.out.println("selectedSources = " + selectedSources);
+        String email = subscriptionForm.getEmail();
+        Member member = Member.createMember(name, email);
+        List<Source> selectedSources = getSelectedSources(subscriptionForm);
+
+        memberService.join(member);
+        subscriptionService.subscribe(member, selectedSources);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/subscriptions/find")
+    public String getFind(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "subscription/find";
+    }
+
+    @PostMapping("/subscriptions/find")
+    public String findMember(@ModelAttribute MemberForm memberForm, RedirectAttributes redirectAttributes) {
+        String name = memberForm.getName();
+        String email = memberForm.getEmail();
+
+        Member member = Member.createMember(name, email);
+        Optional<Member> memberOptional = memberService.find(member);
+        Member findMember = memberOptional.orElseGet(() -> member);
+
+        // 찾은 회원이 존재하지 않음
+        if (findMember.getId() == null) {
+            return "redirect:/subscriptions/not-found";
+        }
+
+        redirectAttributes.addAttribute("status", true);
+        redirectAttributes.addAttribute("email", email);
+        redirectAttributes.addAttribute("name", name);
+
+        return "redirect:/subscriptions/{email}";
+    }
+
+    @GetMapping("/subscriptions/{email}")
+    public String showSingle(@PathVariable String email, @RequestParam String name, Model model) {
+        Member member = Member.createMember(name, email);
+        Optional<Member> memberOptional = memberService.find(member);
+        Member findMember = memberOptional.orElseGet(() -> member);
+
+        model.addAttribute("member", findMember);
+
+        return "/subscription/single";
+    }
+
+    private List<Source> getSelectedSources(SubscriptionForm subscriptionForm) {
+        List<Long> selectedSources = subscriptionForm.getSelectedSources();
+        Map<Long, Source> sourceMap = sourceRepository.findAllMap();
+        List<Source> selectedSourcesList = new ArrayList<>();
+
+        for (Long sourceId : selectedSources) {
+            Source source = sourceMap.get(sourceId);
+            selectedSourcesList.add(source);
+        }
+
+        return selectedSourcesList;
     }
 }

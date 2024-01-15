d124e8be8438adfddc0d5f6dd45b8b488657cf45
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:36:38 +0900

    (test) 구독 출처 레포지토리 테스트 작성
    - 출처 레포지토리의 출처 전체 조회 테스트 작성

--- src/test/java/kauproject/kaunotifier/repository/SourceRepositoryTest.java
+++ src/test/java/kauproject/kaunotifier/repository/SourceRepositoryTest.java
@@ -1,2 +1,29 @@
-package kauproject.kaunotifier.repository;public class SourceRepositoryTest {
+package kauproject.kaunotifier.repository;
+
+import jakarta.transaction.Transactional;
+import kauproject.kaunotifier.domain.Member;
+import kauproject.kaunotifier.domain.Source;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class SourceRepositoryTest {
+
+    @Autowired SourceRepository sourceRepository;
+
+    @Test
+    void findAll() {
+        // given
+        List<Source> sourceList = sourceRepository.findAll();
+
+        assertThat(sourceList.size()).isGreaterThan(0);
+    }
 }

8318368cdd7ea96f60938edaf860f1d8e9d47675
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:24:25 +0900

    (feat) 출처 레포지토리 구현
    - JPA를 사용한 데이터베이스 접근
    - 출처 정보는 웹 서비스에서 변경되지 않을 예정이기에 단순 조회 기능만
      구현하였습니다.

--- src/main/java/kauproject/kaunotifier/repository/SourceRepository.java
+++ src/main/java/kauproject/kaunotifier/repository/SourceRepository.java
@@ -1,2 +1,23 @@
-package kauproject.kaunotifier.repository;public class SourceRepository {
+package kauproject.kaunotifier.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import kauproject.kaunotifier.domain.Source;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class SourceRepository {
+
+    private final EntityManager em;
+
+    public List<Source> findAll() {
+        return em.createQuery("select s from Source s", Source.class)
+                .getResultList();
+    }
 }

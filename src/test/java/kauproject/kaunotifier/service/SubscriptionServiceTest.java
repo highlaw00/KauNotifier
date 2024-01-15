dd15b9c64a881c7c28d56caf99daaee78b5747da
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 11:55:43 +0900

    (feat) SubscriptionService 테스트 구현
    - 환경: H2 데이터베이스
    - 테스트 기능:
    - 구독 기능

---
+++ src/test/java/kauproject/kaunotifier/service/SubscriptionServiceTest.java
@@ -0,0 +1,47 @@
+package kauproject.kaunotifier.service;
+
+import jakarta.transaction.Transactional;
+import kauproject.kaunotifier.domain.Member;
+import kauproject.kaunotifier.domain.Source;
+import kauproject.kaunotifier.domain.Subscription;
+import kauproject.kaunotifier.repository.MemberRepository;
+import kauproject.kaunotifier.repository.SourceRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class SubscriptionServiceTest {
+
+    @Autowired private SubscriptionService subscriptionService;
+    @Autowired private SourceRepository sourceRepository;
+    @Autowired private MemberService memberService;
+
+    @Test
+    void subscribe() {
+        // given
+        Member testA = Member.createMember("테스트 멤버", "test123@gmail.com");
+        memberService.join(testA);
+        List<Source> sourceList = sourceRepository.findAll();
+        int sourceSize = sourceList.size();
+
+        List<Subscription> beforeSubscriptionList = subscriptionService.findSubscriptionOfMember(testA);
+        int beforeSize = beforeSubscriptionList.size();
+
+        // when
+        subscriptionService.subscribe(testA, sourceList);
+        List<Subscription> afterSubscriptionList = subscriptionService.findSubscriptionOfMember(testA);
+        int afterSize = afterSubscriptionList.size();
+
+        // then
+        assertThat(afterSize).isEqualTo(beforeSize + sourceSize);
+
+    }
+
+}

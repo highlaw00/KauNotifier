0b0da316c75b933b16eb2c6375c6c5c9c8e2e8e8
최율(***9***069) <choiyool00@kau.kr>
2024-01-15 12:11:11 +0900

    (feat) 구독 화면 컨트롤러 작성

--- src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
+++ src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
@@ -1,7 +1,41 @@
 package kauproject.kaunotifier.controller;
 
+import kauproject.kaunotifier.domain.Source;
+import kauproject.kaunotifier.repository.SourceRepository;
+import kauproject.kaunotifier.service.MemberService;
+import kauproject.kaunotifier.service.SubscriptionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class SubscribeController {
+
+    private final SubscriptionService subscriptionService;
+    private final MemberService memberService;
+    private final SourceRepository sourceRepository;
+
+    @GetMapping("subscribe")
+    public String subscribeHome(Model model) {
+        List<Source> sourceList = sourceRepository.findAll();
+        model.addAttribute("sources", sourceList);
+        model.addAttribute("subscriptionForm", new SubscriptionForm());
+        return "subscription/subscribe";
+    }
+
+    @PostMapping("subscribe")
+    public String subscribe(@ModelAttribute SubscriptionForm subscriptionForm) {
+        String name = subscriptionForm.getName();
+        List<String> selectedSources = subscriptionForm.getSelectedSources();
+        System.out.println("name = " + name);
+        System.out.println("selectedSources = " + selectedSources);
+
+        return "redirect:/";
+    }
 }

371861465ebe2bb1484768e0aa26381c5f90371e
최율(***9***069) <choiyool00@kau.kr>
2024-02-16 19:41:05 +0900

    (chore) Cors 예외에 CloudFront 도메인 추가

--- src/main/java/kauproject/kaunotifier/filter/CorsFilter.java
+++ src/main/java/kauproject/kaunotifier/filter/CorsFilter.java
@@ -21,6 +21,7 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Origin", "http://kau-notifier.site");
+        response.setHeader("Access-Control-Allow-Origin", "https://d1cmua0u95ro8f.cloudfront.net");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");

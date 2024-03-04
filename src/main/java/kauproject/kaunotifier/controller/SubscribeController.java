a0ab6003682fed73f881b2bdac47992e48995060
Highlaw <choiyool00@gmail.com>
2024-03-04 19:09:00 +0900

    (chore) MVC 패턴 코드 삭제
    - MVC 아키텍처를 위한 코드를 삭제하였습니다.

--- src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
+++ src/main/java/kauproject/kaunotifier/controller/SubscribeController.java
@@ -45,76 +45,76 @@ public class SubscribeController {
         dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 
-    @GetMapping("subscriptions/subscribe")
-    public String subscribeHome(Model model) {
-        SubscriptionForm subscriptionForm = new SubscriptionForm();
-        subscriptionForm.setSourceMap(sourceRepository.findAllMap());
-        model.addAttribute("subscriptionForm", subscriptionForm);
-        return "subscription/subscribe";
-    }
+//    @GetMapping("subscriptions/subscribe")
+//    public String subscribeHome(Model model) {
+//        SubscriptionForm subscriptionForm = new SubscriptionForm();
+//        subscriptionForm.setSourceMap(sourceRepository.findAllMap());
+//        model.addAttribute("subscriptionForm", subscriptionForm);
+//        return "subscription/subscribe";
+//    }
 
-    @PostMapping("subscriptions/subscribe")
-    public String subscribe(@Validated @ModelAttribute SubscriptionForm subscriptionForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+//    @PostMapping("subscriptions/subscribe")
+//    public String subscribe(@Validated @ModelAttribute SubscriptionForm subscriptionForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+//
+//        String name = subscriptionForm.getName();
+//        String email = subscriptionForm.getEmail();
+//        Member member = Member.createMember(name, email);
+//        List<Source> selectedSources = getSelectedSources(subscriptionForm);
+//
+//        // 중복 회원 검증 로직
+//        if (memberService.isDuplicatedMember(member)) {
+//            bindingResult.addError(new ObjectError("memberErr", "이미 등록된 이메일입니다."));
+//        }
+//
+//        // Source Validation
+//        if (selectedSources.isEmpty()) {
+//            bindingResult.addError(new ObjectError("sourcesError", "공지사항 출처를 하나 이상 선택해주세요."));
+//        }
+//
+//        // BindingResult 사용
+//        if (bindingResult.hasErrors()) {
+//            subscriptionForm.setSourceMap(sourceRepository.findAllMap());
+//            model.addAttribute("subscriptionForm", subscriptionForm);
+//            return "subscription/subscribe";
+//        }
+//
+//        // 성공 로직 -> 저장을 일단하기
+//        memberService.join(member);
+//        subscriptionService.subscribe(member, selectedSources);
+//        redirectAttributes.addAttribute("email", email);
+//        redirectAttributes.addAttribute("name", name);
+//        redirectAttributes.addAttribute("status", true);
+//
+//        return "redirect:/subscriptions/{email}";
+//    }
 
-        String name = subscriptionForm.getName();
-        String email = subscriptionForm.getEmail();
-        Member member = Member.createMember(name, email);
-        List<Source> selectedSources = getSelectedSources(subscriptionForm);
-
-        // 중복 회원 검증 로직
-        if (memberService.isDuplicatedMember(member)) {
-            bindingResult.addError(new ObjectError("memberErr", "이미 등록된 이메일입니다."));
-        }
-
-        // Source Validation
-        if (selectedSources.isEmpty()) {
-            bindingResult.addError(new ObjectError("sourcesError", "공지사항 출처를 하나 이상 선택해주세요."));
-        }
-
-        // BindingResult 사용
-        if (bindingResult.hasErrors()) {
-            subscriptionForm.setSourceMap(sourceRepository.findAllMap());
-            model.addAttribute("subscriptionForm", subscriptionForm);
-            return "subscription/subscribe";
-        }
-
-        // 성공 로직 -> 저장을 일단하기
-        memberService.join(member);
-        subscriptionService.subscribe(member, selectedSources);
-        redirectAttributes.addAttribute("email", email);
-        redirectAttributes.addAttribute("name", name);
-        redirectAttributes.addAttribute("status", true);
-
-        return "redirect:/subscriptions/{email}";
-    }
-
-    @GetMapping("subscriptions/find")
-    public String getFind(Model model) {
-        model.addAttribute("memberForm", new MemberForm());
-        return "subscription/find";
-    }
-
-    @PostMapping("subscriptions/find")
-    public String findMember(@Validated @ModelAttribute MemberForm memberForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        String name = memberForm.getName();
-        String email = memberForm.getEmail();
-
-        if (memberService.find(Member.createMember(name, email)).isEmpty()) {
-            bindingResult.addError(new ObjectError("memberErr", "존재하지 않거나 이름과 이메일이 맞지 않습니다."));
-        }
-
-        if (bindingResult.hasErrors()) {
-            // BindingResult는 자동으로 뷰에 넘어감!
-            // redirect를 하는것이 아니다...
-            return "subscription/find";
-        }
-
-        redirectAttributes.addAttribute("status", true);
-        redirectAttributes.addAttribute("email", email);
-        redirectAttributes.addAttribute("name", name);
-
-        return "redirect:/subscriptions/{email}";
-    }
+//    @GetMapping("subscriptions/find")
+//    public String getFind(Model model) {
+//        model.addAttribute("memberForm", new MemberForm());
+//        return "subscription/find";
+//    }
+//
+//    @PostMapping("subscriptions/find")
+//    public String findMember(@Validated @ModelAttribute MemberForm memberForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+//        String name = memberForm.getName();
+//        String email = memberForm.getEmail();
+//
+//        if (memberService.find(Member.createMember(name, email)).isEmpty()) {
+//            bindingResult.addError(new ObjectError("memberErr", "존재하지 않거나 이름과 이메일이 맞지 않습니다."));
+//        }
+//
+//        if (bindingResult.hasErrors()) {
+//            // BindingResult는 자동으로 뷰에 넘어감!
+//            // redirect를 하는것이 아니다...
+//            return "subscription/find";
+//        }
+//
+//        redirectAttributes.addAttribute("status", true);
+//        redirectAttributes.addAttribute("email", email);
+//        redirectAttributes.addAttribute("name", name);
+//
+//        return "redirect:/subscriptions/{email}";
+//    }
 
 //    @GetMapping("subscriptions/{email}")
 //    public String showSingle(@PathVariable String email, @RequestParam String name, Model model) {
@@ -131,96 +131,96 @@ public class SubscribeController {
 //        return "subscription/single";
 //    }
 
-    // TODO: Refactor this.
-    @GetMapping("subscriptions/{email}/edit")
-    public String getEditSubscriptions(@PathVariable String email, @RequestParam String name, Model model) {
-        Member member = Member.createMember(name, email);
-        Optional<Member> memberOptional = memberService.find(member);
-        if (memberOptional.isEmpty()) {
-            return "redirect:/subscriptions/not-found";
-        }
-
-        List<Source> sourceList = sourceRepository.findAllList();
-
-        SubscriptionForm subscriptionForm = new SubscriptionForm();
-        subscriptionForm.setName(name);
-        subscriptionForm.setEmail(email);
-        subscriptionForm.setSources(sourceList);
-
-        model.addAttribute("sources", sourceList);
-        model.addAttribute("subscriptionForm", subscriptionForm);
-        return "subscription/edit";
-    }
-
-    @PostMapping("subscriptions/{email}/edit")
-    public String editSubscriptions(@PathVariable String email, @Validated @ModelAttribute SubscriptionForm subscriptionForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
-
-        String name = subscriptionForm.getName();
-        Member member = Member.createMember(name, email);
-        List<Source> selectedSources = getSelectedSources(subscriptionForm);
-
-        // 존재 검증
-        if (memberService.find(member).isEmpty()) {
-            bindingResult.addError(new ObjectError("memberErr", "존재하지 않거나 이름과 이메일이 맞지 않습니다."));
-        }
-
-        // 출처 선택 검증
-        if (selectedSources.isEmpty()) {
-            bindingResult.addError(new ObjectError("sourcesError", "공지사항 출처를 하나 이상 선택해주세요."));
-        }
-
-        if (bindingResult.hasErrors()) {
-            subscriptionForm.setSources(sourceRepository.findAllList());
-            model.addAttribute("subscriptionForm", subscriptionForm);
-            return "subscription/edit";
-        }
-
-        // 성공 로직
-        member = memberService.find(member).get();
-        subscriptionService.updateSubscribe(member, selectedSources);
-
-        redirectAttributes.addAttribute("status", true);
-        redirectAttributes.addAttribute("email", email);
-        redirectAttributes.addAttribute("name", name);
-
-        return "redirect:/subscriptions/{email}";
-    }
-
-    @GetMapping("subscriptions/{email}/quit")
-    public String getQuit(@PathVariable String email, @RequestParam String name, Model model) {
-        Member member = Member.createMember(name, email);
-        Optional<Member> memberOptional = memberService.find(member);
-        if (memberOptional.isEmpty()) {
-            return "redirect:/subscriptions/not-found";
-        }
-
-        member = memberOptional.get();
-
-        SubscriptionQuitForm subscriptionQuitForm = new SubscriptionQuitForm();
-        subscriptionQuitForm.setName(name);
-        subscriptionQuitForm.setEmail(email);
-        System.out.println("subscriptionQuitForm.getName() = " + subscriptionQuitForm.getName());
-        subscriptionQuitForm.setSubscriptionList(subscriptionService.findSubscriptionOfMember(member));
-
-        model.addAttribute("subscriptionQuitForm", subscriptionQuitForm);
-        return "subscription/quit";
-    }
-
-    @PostMapping("subscriptions/{email}/quit")
-    public String quit(@PathVariable String email, @ModelAttribute SubscriptionQuitForm quitForm) {
-
-        // TODO: Validate member
-        Member member = memberService.find(Member.createMember(quitForm.getName(), email)).get();
-        subscriptionService.unsubscribe(member);
-        memberService.quit(member);
-
-        return "redirect:/";
-    }
-
-    @GetMapping("subscriptions/not-found")
-    public String notFound() {
-        return "subscription/not-found";
-    }
+//    // TODO: Refactor this.
+//    @GetMapping("subscriptions/{email}/edit")
+//    public String getEditSubscriptions(@PathVariable String email, @RequestParam String name, Model model) {
+//        Member member = Member.createMember(name, email);
+//        Optional<Member> memberOptional = memberService.find(member);
+//        if (memberOptional.isEmpty()) {
+//            return "redirect:/subscriptions/not-found";
+//        }
+//
+//        List<Source> sourceList = sourceRepository.findAllList();
+//
+//        SubscriptionForm subscriptionForm = new SubscriptionForm();
+//        subscriptionForm.setName(name);
+//        subscriptionForm.setEmail(email);
+//        subscriptionForm.setSources(sourceList);
+//
+//        model.addAttribute("sources", sourceList);
+//        model.addAttribute("subscriptionForm", subscriptionForm);
+//        return "subscription/edit";
+//    }
+//
+//    @PostMapping("subscriptions/{email}/edit")
+//    public String editSubscriptions(@PathVariable String email, @Validated @ModelAttribute SubscriptionForm subscriptionForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+//
+//        String name = subscriptionForm.getName();
+//        Member member = Member.createMember(name, email);
+//        List<Source> selectedSources = getSelectedSources(subscriptionForm);
+//
+//        // 존재 검증
+//        if (memberService.find(member).isEmpty()) {
+//            bindingResult.addError(new ObjectError("memberErr", "존재하지 않거나 이름과 이메일이 맞지 않습니다."));
+//        }
+//
+//        // 출처 선택 검증
+//        if (selectedSources.isEmpty()) {
+//            bindingResult.addError(new ObjectError("sourcesError", "공지사항 출처를 하나 이상 선택해주세요."));
+//        }
+//
+//        if (bindingResult.hasErrors()) {
+//            subscriptionForm.setSources(sourceRepository.findAllList());
+//            model.addAttribute("subscriptionForm", subscriptionForm);
+//            return "subscription/edit";
+//        }
+//
+//        // 성공 로직
+//        member = memberService.find(member).get();
+//        subscriptionService.updateSubscribe(member, selectedSources);
+//
+//        redirectAttributes.addAttribute("status", true);
+//        redirectAttributes.addAttribute("email", email);
+//        redirectAttributes.addAttribute("name", name);
+//
+//        return "redirect:/subscriptions/{email}";
+//    }
+//
+//    @GetMapping("subscriptions/{email}/quit")
+//    public String getQuit(@PathVariable String email, @RequestParam String name, Model model) {
+//        Member member = Member.createMember(name, email);
+//        Optional<Member> memberOptional = memberService.find(member);
+//        if (memberOptional.isEmpty()) {
+//            return "redirect:/subscriptions/not-found";
+//        }
+//
+//        member = memberOptional.get();
+//
+//        SubscriptionQuitForm subscriptionQuitForm = new SubscriptionQuitForm();
+//        subscriptionQuitForm.setName(name);
+//        subscriptionQuitForm.setEmail(email);
+//        System.out.println("subscriptionQuitForm.getName() = " + subscriptionQuitForm.getName());
+//        subscriptionQuitForm.setSubscriptionList(subscriptionService.findSubscriptionOfMember(member));
+//
+//        model.addAttribute("subscriptionQuitForm", subscriptionQuitForm);
+//        return "subscription/quit";
+//    }
+//
+//    @PostMapping("subscriptions/{email}/quit")
+//    public String quit(@PathVariable String email, @ModelAttribute SubscriptionQuitForm quitForm) {
+//
+//        // TODO: Validate member
+//        Member member = memberService.find(Member.createMember(quitForm.getName(), email)).get();
+//        subscriptionService.unsubscribe(member);
+//        memberService.quit(member);
+//
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("subscriptions/not-found")
+//    public String notFound() {
+//        return "subscription/not-found";
+//    }
 
     @PostMapping("/api/subscribe")
     public SubscribeDto subscribe(@Valid @RequestBody SubscribeDto req, BindingResult result) {
